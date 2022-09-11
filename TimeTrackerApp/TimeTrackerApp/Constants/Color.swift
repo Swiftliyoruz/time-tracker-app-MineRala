6e5d47168e767f0326cacb628b49b4a3f068881d
Mine Rala <mine-rala@hotmail.com>
2022-09-12 00:34:06 +0300

    Added dark mode and font constant.

---
+++ TimeTrackerApp/TimeTrackerApp/Constants/Color.swift
@@ -0,0 +1,12 @@
+//
+//  Color.swift
+//  TimeTrackerApp
+//
+//  Created by Mine Rala on 11.09.2022.
+//
+
+import UIKit
+
+struct Color {
+    static var viewControllerBackgroundColor: UIColor { UITraitCollection.current.userInterfaceStyle == .light ?  #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1) : #colorLiteral(red: 0.03009548783, green: 0.01519311219, blue: 0.09505639225, alpha: 1) }
+}

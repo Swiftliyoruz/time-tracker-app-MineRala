f00cef1448f65c5a8b88deb09dff62ed86aeb62d
Mine Rala <mine-rala@hotmail.com>
2022-09-25 19:59:54 +0300

    some ui changes

--- TimeTrackerApp/TimeTrackerApp/ViewControllers/AddViewController.swift
+++ TimeTrackerApp/TimeTrackerApp/ViewControllers/AddViewController.swift
@@ -23,11 +23,22 @@ final class AddViewController: UIViewController, UINavigationBarDelegate {
     @IBOutlet weak var taskIconButton: UIButton!
     @IBOutlet weak var addButton: UIButton!
     
+    var textAttributesColor = DefaultColor.black
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = AddViewConstant.navigationBarTitle
         tabBarController?.tabBar.items?[1].title = AddViewConstant.tabBarTitle
+        addButton.titleLabel?.font = UIFont(name: Font.regular.rawValue, size: 14)
         setUpUI()
+    }
+    
+    private func setUpNavigationController() {
+        title = AddViewConstant.navigationBarTitle
+        tabBarController?.tabBar.items?.first?.title = AddViewConstant.tabBarTitle
+        navigationController?.navigationBar.prefersLargeTitles = true
+        let textAttributes = [NSAttributedString.Key.foregroundColor: textAttributesColor]
+        navigationController?.navigationBar.titleTextAttributes = textAttributes
     }
     
     private func setUpUI() {
@@ -61,6 +72,20 @@ final class AddViewController: UIViewController, UINavigationBarDelegate {
     
     override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
         self.view.backgroundColor = Color.viewControllerBackgroundColor
+        self.taskTitleLabel.textColor = Color.cellTitleTextColor
+        self.taskTitleTextField.textColor = Color.cellTitleTextColor
+        self.taskTitleTextField.backgroundColor = Color.cellBackgroundColor
+        self.mainCategoryLabel.textColor = Color.cellTitleTextColor
+        self.mainCategoryButton.backgroundColor = Color.cellBackgroundColor
+        self.subCategoryLabel.textColor = Color.cellTitleTextColor
+        self.subCategoryTextField.textColor = Color.cellTitleTextColor
+        self.subCategoryTextField.backgroundColor = Color.cellBackgroundColor
+        self.taskIconLabel.textColor = Color.cellTitleTextColor
+        self.taskIconButton.backgroundColor = Color.cellBackgroundColor
+        self.addButton.titleLabel?.textColor = Color.cellTitleTextColor
+        self.addButton.backgroundColor = Color.cellBackgroundColor
+        
+        
     }
 }
 
